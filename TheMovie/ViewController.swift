5df5219c7a1d7a860087e67493338a390e225360
eduardoSilvestre <eduardo_mizael@outlook.com.br>
2021-10-13 17:27:52 -0300

    Starting Project

---
+++ TheMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by Eduardo Mizael on 13/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
